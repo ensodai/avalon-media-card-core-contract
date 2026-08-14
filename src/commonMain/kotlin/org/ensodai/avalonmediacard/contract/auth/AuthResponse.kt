5b80294e232df1655ae78e212155d623044f53f6
ensodai <takeshimood@gmail.com>
2026-08-14 17:36:23 +0500

    feat(auth): add role to AuthResponse

--- src/commonMain/kotlin/org/ensodai/avalonmediacard/contract/auth/AuthResponse.kt
+++ src/commonMain/kotlin/org/ensodai/avalonmediacard/contract/auth/AuthResponse.kt
@@ -1,10 +1,12 @@
 package org.ensodai.avalonmediacard.contract.auth
 
 import kotlinx.serialization.Serializable
+import org.ensodai.avalonmediacard.contract.model.UserRole
 
 @Serializable
 data class AuthResponse(
     val success: Boolean,
     val token: String? = null,
-    val error: String? = null
+    val error: String? = null,
+    val role: UserRole? = null
 )
